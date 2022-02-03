6ae65abc5ebaa589355d87e6b55d2f8fa366bbfd
tomnaylor <tom.naylor@hotmail.com>
2022-02-03 21:56:59 +0000

    setup allauth login

--- opera_rep/settings.py
+++ opera_rep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*-f$((*lc8mwx!yf%y)^oi03ii$plftwkx3u3a&%zwaa!0#n@'
+SECRET_KEY = 'django-insecure-@*-f$((*lc8mwx!yf%y)^oi03ii$plftwkx3u3a&%zwaa!0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +80,17 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
 
 WSGI_APPLICATION = 'opera_rep.wsgi.application'
 
