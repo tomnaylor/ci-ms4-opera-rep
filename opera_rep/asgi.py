38886ebcf9510be4a3d9cc2967be317aced97718
tomnaylor <tom.naylor@hotmail.com>
2022-02-03 21:22:48 +0000

    initial commit

---
+++ opera_rep/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for opera_rep project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opera_rep.settings')
+
+application = get_asgi_application()
