0582e70745cfab2b1dc8a33daae17b220c2d9652
tomnaylor <tom.naylor@hotmail.com>
2022-02-04 22:27:29 +0000

    add: works app with work and product models

---
+++ works/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class Work(models.Model):
+    """ Model for the work """
+
+    url = models.CharField(max_length=254, blank=False)
+    name = models.CharField(max_length=254, blank=False)
+    synopsis = models.TextField()
+    tagline = models.CharField(max_length=254, blank=False)
+    hash_tag = models.CharField(max_length=30, blank=False)
+    language = models.CharField(max_length=100, blank=False)
+    composer = models.CharField(max_length=100, blank=False)
+    librettist = models.CharField(max_length=100, blank=False)
+    world_premiere = models.DateField(auto_now=False, auto_now_add=False)
+    hero_image = models.ImageField(null=True, blank=True)
+    hero_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    thumb_image = models.ImageField(null=True, blank=True)
+    thumb_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    record_added = models.DateTimeField(auto_now_add=True)
+    record_edited = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Production(models.Model):
+    """ Model for a production """
+
+    work = models.ForeignKey('Work', null=True, blank=True, on_delete=models.SET_NULL)
+    year = models.PositiveSmallIntegerField()
+    production_premiere = models.DateField(auto_now=False, auto_now_add=False)
+    director = models.CharField(max_length=100, null=True, blank=True)
+    set_designer = models.CharField(max_length=100, null=True, blank=True)
+    costume_designer = models.CharField(max_length=100, null=True, blank=True)
+    lighting_designer = models.CharField(max_length=100, null=True, blank=True)
+    relighter = models.CharField(max_length=100, null=True, blank=True)
+    choreographer = models.CharField(max_length=100, null=True, blank=True)
+    producer = models.CharField(max_length=100, null=True, blank=True)
+
+    dead = models.BooleanField(default=False, null=True, blank=True)
+
+    hero_image = models.ImageField(null=True, blank=True)
+    hero_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    thumb_image = models.ImageField(null=True, blank=True)
+    thumb_image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    record_added = models.DateTimeField(auto_now_add=True)
+    record_edited = models.DateTimeField(auto_now=True)
+
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
