c276a5ff45e525a1ff6689f7967314fab0d4fadf
tomnaylor <tom.naylor@hotmail.com>
2022-05-16 14:01:31 +0000

    add: AWS storage and remote Mongo DB

--- opera_rep/settings.py
+++ opera_rep/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+# from decouple import config
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*-f$((*lc8mwx!yf%y)^oi03ii$plftwkx3u3a&%zwaa!0#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'tn83-wno-test-bed.herokuapp.com']
 
 
 # Application definition
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
     'profiles',
     'crispy_forms',
     'crispy_bootstrap5',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -117,12 +120,30 @@ WSGI_APPLICATION = 'opera_rep.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    # if 'DEVELOPMENT' not in os.environ:
+    # if 1 == 1:
+    print()
+    print('----------------------------------')
+    print('DATABASE_URL FOUND - USING EXTERNAL DB')
+    print('' + os.environ['DATABASE_URL'][0:50] + '...###')
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ['DATABASE_URL'])
     }
-}
+
+else:
+    
+    print()
+    print('----------------------------------')
+    print('DATABASE_URL NOT FOUND - KILLING APP')
+    import sys
+    sys.exit(0)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -130,16 +151,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'NumericPasswordValidator',
     },
 ]
 
@@ -148,13 +173,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -169,33 +190,65 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# AWS
+if 'AWS_ACCESS_KEY_ID' in os.environ and 'AWS_SECRET_ACCESS_KEY' in os.environ:
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    AWS_STORAGE_BUCKET_NAME = 'wno-test-bed'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_ENDPOINT_URL = 'https://s3.eu-west-2.amazonaws.com'
+
+    # STATIC AND MEDIA FILE LOCATIONS
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+    print()
+    print('----------------------------------')
+    print('USING AWS STORAGE:')
+    print('KEY: ' + AWS_ACCESS_KEY_ID[0:5] + '########')
+    print('SECRET: ' + AWS_SECRET_ACCESS_KEY[0:5] + '########')
+    print('STATIC URL: ' + STATIC_URL)
+    print('MEDIA URL: ' + MEDIA_URL)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-if 'DEVELOPMENT' in os.environ:
+# if 'DEVELOPMENT' in os.environ:
+if 1 == 1:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'wno-no-reply@example.com'
-    print("-------------------DEVELOPMENT ENV IN SETTINGS OS")
+    print()
+    print('----------------------------------')
+    print("DEVELOPMENT MODE")
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    #EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-    #EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASS')
-    #DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
-    print("-----------------PROUCTION ENV IN SETTINGS OS")
+    # EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASS')
+    # DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+    print()
+    print('----------------------------------')
+    print("PROUCTION MODE")
 
 
-# TEST BITS
-# SET ENV VARS USING gp env FOO=bar
-print(os.environ['DEVELOPMENT'])
-"""
-API_USERNAME = config('USERNAMETOM')
-API_KEY = config('KEY')
-print(API_USERNAME)
-print(API_KEY)
-"""
+print()
+print()
