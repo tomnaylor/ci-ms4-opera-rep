875bb5ede3081983adccabc7d4741543077030a0
tomnaylor <tom.naylor@hotmail.com>
2022-05-23 15:29:19 +0000

    fix: email updates

--- opera_rep/settings.py
+++ opera_rep/settings.py
@@ -250,11 +250,12 @@ print()
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = False
-EMAIL_PORT = 25
+EMAIL_TIMEOUT = 4
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.sendgrid.net'
 DEFAULT_FROM_EMAIL = 'tomnaylor@gmail.com'
+SERVER_EMAIL = 'tomnaylor@gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
