fb4c4694a6e4c538b6e5d43e7f2c9d3c97fe7040
tomnaylor <tom.naylor@hotmail.com>
2022-05-26 14:07:47 +0000

    fix: profile model

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,33 +7,34 @@ class UserProfileForm(forms.ModelForm):
     
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        fields = ['city','country']
+    #     exclude = ('user',)
 
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
+    # def __init__(self, *args, **kwargs):
+    #     """
+    #     Add placeholders and classes, remove auto-generated
+    #     labels and set autofocus on first field
+    #     """
         
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'city': 'Town or City',
-            'country': 'Country',
-        }
+    #     super().__init__(*args, **kwargs)
+    #     placeholders = {
+    #         'city': 'Town or City',
+    #         'country': 'Country',
+    #     }
 
         
-        for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
+    #     for field in self.fields:
+    #         if field != 'country':
+    #             if self.fields[field].required:
+    #                 placeholder = f'{placeholders[field]} *'
+    #             else:
+    #                 placeholder = placeholders[field]
 
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = ('border-black '
-                                                        'rounded-0 '
-                                                        'profile-form-input')
-            self.fields[field].label = False
+    #             self.fields[field].widget.attrs['placeholder'] = placeholder
+    #         self.fields[field].widget.attrs['class'] = ('border-black '
+    #                                                     'rounded-0 '
+    #                                                     'profile-form-input')
+    #         self.fields[field].label = False
 
 
 class ProductionCommentForm(forms.ModelForm):
