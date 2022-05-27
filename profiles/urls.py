896117182e6092568c9e28a11dc56c27a4b447a0
tomnaylor <tom.naylor@hotmail.com>
2022-05-27 14:53:05 +0000

    fix: missing c from comment_id

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('comment-delete/<int:comment_id>/', views.comment_remove, name='comment_remove'),
-    path('comment-edit/<int:omment_id>/', views.comment_edit, name='comment_edit'),
+    path('comment-edit/<int:comment_id>/', views.comment_edit, name='comment_edit'),
     path('comment-add/<int:prod_id>/', views.comment_add, name='comment_add'),
 ]
