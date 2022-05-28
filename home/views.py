7ca4438a011c746978e951d2d35ef4cea70413a9
tomnaylor <tom.naylor@hotmail.com>
2022-05-28 00:35:38 +0000

    fix:pep8 fixes

--- home/views.py
+++ home/views.py
@@ -3,19 +3,21 @@ from django.db.models import Sum, Count
 from donations.models import Donation
 from profiles.models import UserLike, UserComment
 from works.models import (
-                     Work,
                      Production,
                      ProductionVideo,
-                     Role,
-                     People,
                      ProductionPhoto)
 
 
 def index(request):
     """ A view to return index page """
 
-    four_most_liked_id = UserLike.objects.values('production_id').annotate(production_count=Count('production_id')).order_by('-production_count')[:4]
-    four_most_liked = Production.objects.filter(pk__in=[item['production_id'] for item in four_most_liked_id])
+    four_most_liked_id = UserLike.objects.values(
+        'production_id').annotate(
+            production_count=Count('production_id')).order_by(
+                '-production_count')[:4]
+
+    four_most_liked = Production.objects.filter(
+        pk__in=[item['production_id'] for item in four_most_liked_id])
 
     user_comments = UserComment.objects.filter().order_by('-record_added')[:8]
 
@@ -23,11 +25,10 @@ def index(request):
 
     prod_videos = ProductionVideo.objects.order_by('-record_added')[:4]
     prod_photos = ProductionPhoto.objects.order_by('-record_added')[:10]
-    
+
     productions = Production.objects.all().order_by(
         '-production_premiere',
         'work__name')
-
 
     context = {
         'four_most_liked': four_most_liked,
